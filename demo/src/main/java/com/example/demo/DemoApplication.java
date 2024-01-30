1485323e98b7a61deee051d6a154cc1ccbc973ec
Brian Nyatwanga <briannyatwanga@gmail.com>
2024-01-30 14:55:00 +0300

    Added swagger ui

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+
 
 
 @SpringBootApplication
+//@EnableSwagger2
+@EnableSwagger2
 public class DemoApplication {
 
 	public static void main(String[] args) {
